3a4364a077ff90ed5a3a0dd13c2e4c801413968b
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2024-03-07 01:39:07 -0500

    Updated documentation

--- 4.1.0/build.sh
+++ 4.1.0/build.sh
@@ -3,7 +3,7 @@
 # Copyright © 2021-2024 Pittsburgh Supercomputing Center.
 # All Rights Reserved.
 
-IMAGE=apptainer-singularityce-4.1.sif
+IMAGE=apptainer-singularityce-4.1.0.sif
 DEFINITION=recipe.def
 
 apptainer build -F $IMAGE $DEFINITION
